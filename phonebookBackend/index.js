3398ed3fe3b8b3614f8700cd51dfc2c9c3556c71
joeyb <jbcassid@mtu.edu>
2023-12-18 15:33:06 -0500

    3.17 put update

--- phonebookBackend/index.js
+++ phonebookBackend/index.js
@@ -20,28 +20,6 @@ app.use(
   )
 );
 
-let phoneBook = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
